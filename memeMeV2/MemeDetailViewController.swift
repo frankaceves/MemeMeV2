cb56ded5227d8c768f1cc77aa3f647db570188d5
Frank Aceves <frankaceves@gmail.com>
2018-02-10 11:52:05 -0800

    add Meme Detail View Controller to storyboard.  add MemeDetailViewController class swift file

---
+++ memeMeV2/MemeDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe v2.0
+//
+//  Created by Frank Anthony Aceves on 2/10/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
